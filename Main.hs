1579d9fde2f707002f476f27641b033da344e2df
Oliver Charles <ollie@ocharles.org.uk>
2018-01-18 21:03:21 +0000

    Formatting

--- Main.hs
+++ Main.hs
@@ -1,27 +1,36 @@
+{-# language NamedFieldPuns #-}
+
 module Main (main) where
 
 import Data.Function ( (&) )
 import System.Environment ( getArgs )
-import Options.Applicative
 
 import Distribution.Package.Dhall
 
 import qualified Data.Text.Lazy.IO as LazyText
 import qualified Dhall
 import qualified Distribution.PackageDescription.PrettyPrint as Cabal
+import qualified Options.Applicative as OptParse
 
 data Options = Options { dhallFilePath :: String }
 
 run :: Options -> IO ()
-run (Options dhallFilePath) = 
-    LazyText.readFile dhallFilePath
-    >>= Dhall.detailed . Dhall.input packageDescription
-    & fmap Cabal.showPackageDescription
-    >>= putStrLn
+run Options { dhallFilePath } =
+  LazyText.readFile dhallFilePath
+   >>= Dhall.detailed . Dhall.input packageDescription
+   & fmap Cabal.showPackageDescription
+   >>= putStrLn
 
 main :: IO ()
-main = execParser opts >>= run
-    where
-        parser = Options <$> argument str (metavar "<dhall input file>")
-        opts = info parser mempty
+main =
+  OptParse.execParser opts >>= run
 
+  where
+
+  parser =
+    Options
+      <$> OptParse.argument
+            OptParse.str ( OptParse.metavar "<dhall input file>" )
+
+  opts =
+    OptParse.info parser mempty
