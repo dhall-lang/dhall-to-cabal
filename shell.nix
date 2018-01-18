aaba1fd11652956fed58e9e5bd4d848a2aac1096
Tobias Pflug <tobias.pflug@holidaycheck.com>
2018-01-18 22:14:50 +0100

    Use callcabal2nix

--- shell.nix
+++ shell.nix
@@ -1,29 +1,3 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "ghc822" }:
 
-let
-
-  inherit (nixpkgs) pkgs;
-
-  f = { mkDerivation, base, Cabal, containers, dhall, stdenv
-      , tasty-golden, text
-      }:
-      mkDerivation {
-        pname = "dhall-to-cabal";
-        version = "0.1.0";
-        src = ./.;
-        isLibrary = false;
-        isExecutable = true;
-        executableHaskellDepends = [ base Cabal containers dhall text ];
-        testHaskellDepends = [ tasty-golden ];
-        license = "unknown";
-      };
-
-  haskellPackages = if compiler == "default"
-                       then pkgs.haskellPackages
-                       else pkgs.haskell.packages.${compiler};
-
-  drv = haskellPackages.callPackage f {};
-
-in
-
-  if pkgs.lib.inNixShell then drv.env else drv
+(nixpkgs.haskell.packages.${compiler}.callCabal2nix "dhall-to-cabal" ./. {}).env
