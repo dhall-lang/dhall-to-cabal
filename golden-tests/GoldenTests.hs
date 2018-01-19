8cf4f5e2798c34c5525e334a86dead78655eb891
Ollie Charles <ollie@ocharles.org.uk>
2018-01-19 16:26:10 +0000

    Update the golden tests to be a bit more lenient

--- golden-tests/GoldenTests.hs
+++ golden-tests/GoldenTests.hs
@@ -1,13 +1,17 @@
 -- Taken from https://ro-che.info/articles/2017-12-04-golden-tests
 
 import qualified Data.ByteString.Lazy as LBS
-import Distribution.Package.Dhall (dhallFileToCabal)
-import qualified Distribution.PackageDescription.PrettyPrint as Cabal
-import System.FilePath (takeBaseName, replaceExtension)
-import Test.Tasty (defaultMain, TestTree, testGroup)
-import Test.Tasty.Golden (goldenVsString, findByExtension)
 import qualified Data.Text.Lazy as LazyText
 import qualified Data.Text.Lazy.Encoding as LazyText
+import Distribution.Package.Dhall (dhallFileToCabal)
+import qualified Distribution.PackageDescription.Configuration as Cabal
+import qualified Distribution.PackageDescription.Parse as Cabal
+import qualified Distribution.PackageDescription.PrettyPrint as Cabal
+import qualified Distribution.Verbosity as Cabal
+import System.FilePath (takeBaseName, replaceExtension)
+import Test.Tasty (defaultMain, TestTree, testGroup)
+import Test.Tasty.Golden (findByExtension)
+import Test.Tasty.Golden.Advanced (goldenTest)
   
 main :: IO ()
 main = defaultMain =<< goldenTests
@@ -16,10 +20,21 @@ goldenTests :: IO TestTree
 goldenTests = do
   dhallFiles <- findByExtension [".dhall"] "golden-tests"
   return $ testGroup "dhall-to-cabal golden tests"
-    [ goldenVsString
+    [ goldenTest
         (takeBaseName dhallFile) -- test name
-        cabalFile -- golden file path
-        (LazyText.encodeUtf8 . LazyText.pack . Cabal.showPackageDescription <$> dhallFileToCabal dhallFile) -- action whose result is tested
+        (Cabal.flattenPackageDescription <$> Cabal.readGenericPackageDescription Cabal.normal cabalFile)
+        (dhallFileToCabal dhallFile)
+        (\expected actual -> return $
+            if Cabal.showPackageDescription expected == Cabal.showPackageDescription actual then
+              Nothing
+            else
+              Just $
+                "Generated .cabal file does not match input:\n\n" ++
+                "Got:\n\n" ++
+                Cabal.showPackageDescription actual ++
+                "\n\nExpected:\n\n" ++
+                Cabal.showPackageDescription expected)
+        (Cabal.writePackageDescription cabalFile)
     | dhallFile <- dhallFiles
     , let cabalFile = replaceExtension dhallFile ".cabal"
     ]
