15b4f06b0f89dde100eb5d8346c7c66ce1e23726
Oliver Charles <ollie@ocharles.org.uk>
2018-02-02 21:54:23 +0000

    Fix tests and add lens.dhall test

--- golden-tests/GoldenTests.hs
+++ golden-tests/GoldenTests.hs
@@ -1,47 +1,56 @@
--- Taken from https://ro-che.info/articles/2017-12-04-golden-tests
+module Main ( main ) where
 
 import Data.Algorithm.Diff
 import Data.Algorithm.DiffOutput
+import Data.Function ( on )
+import System.FilePath ( takeBaseName, replaceExtension )
+import Test.Tasty ( defaultMain, TestTree, testGroup )
+import Test.Tasty.Golden ( findByExtension )
+import Test.Tasty.Golden.Advanced ( goldenTest )
+
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.Text.Lazy as LazyText
 import qualified Data.Text.Lazy.Encoding as LazyText
-import Distribution.Package.Dhall (dhallFileToCabal)
 import qualified Distribution.PackageDescription.Configuration as Cabal
 import qualified Distribution.PackageDescription.Parse as Cabal
 import qualified Distribution.PackageDescription.PrettyPrint as Cabal
 import qualified Distribution.PackageDescription as Cabal
 import qualified Distribution.Verbosity as Cabal
-import System.FilePath (takeBaseName, replaceExtension)
-import Test.Tasty (defaultMain, TestTree, testGroup)
-import Test.Tasty.Golden (findByExtension)
-import Test.Tasty.Golden.Advanced (goldenTest)
+
+import Distribution.Package.Dhall ( dhallFileToCabal )
+
+
   
 main :: IO ()
-main = defaultMain =<< goldenTests
+main =
+  defaultMain =<< goldenTests
+
+
 
 goldenTests :: IO TestTree
 goldenTests = do
-  dhallFiles <- findByExtension [".dhall"] "golden-tests"
-  return $ testGroup "dhall-to-cabal golden tests"
-    [ goldenTest
-        (takeBaseName dhallFile) -- test name
-        (reverseArtifacts . Cabal.flattenPackageDescription <$> Cabal.readGenericPackageDescription Cabal.normal cabalFile)
-        (dhallFileToCabal dhallFile)
-        (\expected actual -> return $
-            if Cabal.showPackageDescription expected == Cabal.showPackageDescription actual then
-              Nothing
-            else
-              Just $
-                "Generated .cabal file does not match input:\n\n" ++
-                "Differences:\n" ++
-                ppDiff
-                  (getGroupedDiff (lines (Cabal.showPackageDescription actual))
-                                  (lines (Cabal.showPackageDescription expected)))
-        )
-        (Cabal.writePackageDescription cabalFile)
-    | dhallFile <- dhallFiles
-    , let cabalFile = replaceExtension dhallFile ".cabal"
-    ]
+  dhallFiles <-
+    findByExtension [ ".dhall" ] "golden-tests"
+
+  return
+    $ testGroup "dhall-to-cabal golden tests"
+        [ goldenTest
+            ( takeBaseName dhallFile )
+            ( Cabal.readGenericPackageDescription Cabal.normal cabalFile )
+            ( dhallFileToCabal dhallFile )
+            ( \expected actual ->
+                return $
+                  if on (==) Cabal.showGenericPackageDescription expected actual then
+                    Nothing
+                  else
+                    Just "Generated .cabal file does not match input"
+            )
+            ( Cabal.writeGenericPackageDescription cabalFile )
+        | dhallFile <- dhallFiles
+        , let cabalFile = replaceExtension dhallFile ".cabal"
+        ]
+
+
 
 reverseArtifacts pkg =
   pkg { Cabal.executables = reverse (Cabal.executables pkg) }
