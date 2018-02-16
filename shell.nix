81217b5037b425aaeb9e001f1761f4d1fbd39cb9
Ollie Charles <ollie@ocharles.org.uk>
2018-02-16 18:11:46 +0000

    Configuration for Hydra

--- shell.nix
+++ shell.nix
@@ -1,13 +1,1 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc822" }:
-
-((nixpkgs.haskell.packages.${compiler}.override {
-  overrides = self: super: {
-    prettyprinter = self.callPackage ./prettyprinter.nix {};
-
-    dhall = super.callPackage ./dhall.nix {};
-    
-    repline = nixpkgs.haskell.lib.doJailbreak super.repline;
-    
-    formatting = super.callPackage ./formatting.nix {};
-  };
-}).callCabal2nix "dhall-to-cabal" ./. {}).env
+(import ./release.nix {}).dhall-to-cabal.env
