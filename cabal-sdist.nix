fd748806f143bcb26fd7e24233c79a536d778dc1
Oliver Charles <ollie@ocharles.org.uk>
2018-08-10 09:54:24 +0100

    Run the build in the result of cabal sdist

---
+++ cabal-sdist.nix
@@ -0,0 +1,21 @@
+dir:
+let
+  pkgs = import ./pkgs {};
+
+  src-tree = pkgs.stdenv.mkDerivation {
+    name = "src-sdist";
+    buildCommand = ''
+      mkdir -p $out/src
+      cd ${dir}
+      cabal sdist --output-directory=$out/src
+    '';
+    buildInputs = [ pkgs.cabal-install ];
+  };
+
+in
+import
+  ( pkgs.stdenv.mkDerivation {
+      name = "src.nix";
+      buildCommand = ''echo "${src-tree}"/src > $out'';
+    }
+  )
