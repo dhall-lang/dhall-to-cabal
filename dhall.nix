455ab4625011cddab39ef6a8b6dd1eee2c3d144a
Gabriel Gonzalez <Gabriel439@gmail.com>
2018-10-15 23:13:54 -0700

    Build against `dhall-1.18.0` on Hackage now

--- dhall.nix
+++ dhall.nix
@@ -1,8 +1,8 @@
 { mkDerivation, ansi-terminal, base, bytestring, case-insensitive
 , cborg, containers, contravariant, criterion, cryptonite, deepseq
-, Diff, directory, doctest, exceptions, fetchgit, filepath
-, haskeline, http-client, http-client-tls, lens-family-core
-, megaparsec, memory, mockery, mtl, optparse-applicative, parsers
+, Diff, directory, doctest, exceptions, filepath, haskeline
+, http-client, http-client-tls, lens-family-core, megaparsec
+, memory, mockery, mtl, optparse-applicative, parsers
 , prettyprinter, prettyprinter-ansi-terminal, QuickCheck
 , quickcheck-instances, repline, scientific, serialise, stdenv
 , tasty, tasty-hunit, tasty-quickcheck, template-haskell, text
@@ -10,12 +10,8 @@
 }:
 mkDerivation {
   pname = "dhall";
-  version = "1.17.0";
-  src = fetchgit {
-    url = "https://github.com/dhall-lang/dhall-haskell.git";
-    sha256 = "0vc914162m1cy8rjx38p63i4agkwdrq990j4l422z09gwjcjcd6i";
-    rev = "3737b6fecad671c2e03dc86ce77ccebfad7ee9a6";
-  };
+  version = "1.18.0";
+  sha256 = "b528ecff4f757fa085f61636ffb8a6eb90c761106d7d20bd735bee48a6abab94";
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
