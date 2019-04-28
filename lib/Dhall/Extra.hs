06b625fc12062d84b3e9fc17a6183f24105fd460
quasicomputational <quasicomputational@gmail.com>
2019-04-28 15:58:33 +0100

    Remove the now-unused makeUnion.

--- lib/Dhall/Extra.hs
+++ lib/Dhall/Extra.hs
@@ -5,8 +5,7 @@
 {-# language RecordWildCards #-}
 
 module Dhall.Extra
-  ( makeUnion
-  , validateType
+  ( validateType
   , sortExpr
   )
   where
@@ -15,30 +14,10 @@ import Control.Monad ( join )
 import Data.List ( sortBy )
 import Data.Ord ( comparing )
 
-import qualified Data.Text as StrictText
 import qualified Dhall
 import qualified Dhall.Core as Dhall ( Expr )
 import qualified Dhall.Core as Expr ( Expr(..) )
 import qualified Dhall.Map as Map
-
-
-makeUnion :: Map.Map StrictText.Text ( Dhall.Type a ) -> Dhall.Type a
-makeUnion alts =
-  let
-    extract expr = do
-      Expr.UnionLit ctor v _ <-
-        return expr
-
-      t <-
-        Map.lookup ctor alts
-
-      Dhall.extract t v
-
-    expected =
-      sortExpr ( Expr.Union ( Just . Dhall.expected <$> alts ) )
-
-  in Dhall.Type { .. }
-
 
 
 validateType :: Dhall.Type ( Maybe a ) -> Dhall.Type a
