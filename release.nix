0e612b890453fbef1393c44c8395baa714c68065
quasicomputational <quasicomputational@gmail.com>
2019-08-21 10:06:07 +0100

    Update to newer nixpkgs, drop a whole bunch of overrides. (#184)
    The pinned version is fairly old and there have been some improvements
    to the generic builder since then, which might help with #183. Also
    the package set has more current versions, allowing a whole lot of
    overrides to be culled.
    
    I've moved the remaining one override (for `dhall`) into the
    `overrides/` subdirectory, for neatness and to segregate them from
    infrastructural `.nix` files.

--- release.nix
+++ release.nix
@@ -1,63 +1,18 @@
 let
-  fetchNixpkgs = import ./fetchNixpkgs.nix;
-
-  nixpkgs = fetchNixpkgs {
-    rev = "804060ff9a79ceb0925fe9ef79ddbf564a225d47";
-
-    sha256 = "01pb6p07xawi60kshsxxq1bzn8a0y4s5jjqvhkwps4f5xjmmwav3";
-
-    outputSha256 = "0ga345hgw6v2kzyhvf5kw96hf60mx5pbd9c4qj5q4nan4lr7nkxn";
+  nixpkgs = builtins.fetchGit {
+    url = "https://github.com/NixOS/nixpkgs";
+    rev = "ac827e0e35b8a3f7370ff0dbaea7387dbc514c6c";
   };
 
   config = {
     packageOverrides = pkgs: {
       haskellPackages = pkgs.haskellPackages.override {
         overrides = self: super: {
-          text = self.callPackage ./text.nix {};
-
-          prettyprinter = self.callPackage ./prettyprinter.nix {};
-
-          dhall = pkgs.haskell.lib.dontCheck (super.callPackage ./dhall.nix {});
-
-          repline = super.callPackage ./repline.nix {};
-
-          formatting = super.callPackage ./formatting.nix {};
-
-          megaparsec = super.callPackage ./megaparsec.nix {};
-
-          parser-combinators = super.callPackage ./parser-combinators.nix {};
-
-          tasty-hunit = super.callPackage ./tasty-hunit.nix {};
-
-          serialise = pkgs.haskell.lib.dontCheck super.serialise;
-
-          Cabal = self.callPackage ./cabal.nix {};
-
-          lens = self.callPackage ./lens.nix {};
-
-          cabal-doctest = self.callPackage ./cabal-doctest.nix { Cabal = self.callPackage ./cabal.nix {}; };
-
-          insert-ordered-containers = self.callPackage ./insert-ordered-containers.nix {};
-
+          # Local packages
           dhall-to-cabal = self.callPackage ./dhall-to-cabal.nix {};
 
-          transformers-compat = self.callPackage ./transformers-compat.nix {};
-
-          quickcheck-instances = self.callPackage ./quickcheck-instances.nix {};
-
-          MonadRandom = self.callPackage ./MonadRandom.nix {};
-
-          either = self.callPackage ./either.nix {};
-
-          semigroupoids = self.callPackage ./semigroupoids.nix {};
-
-          base-orphans = self.callPackage ./base-orphans.nix {};
-
-          profunctors = self.callPackage ./profunctors.nix {};
-
-          th-lift-instances = self.callPackage ./th-lift-instances.nix {};
-
-          th-lift = self.callPackage ./th-lift.nix {};
+          # Things we need newer versions of.
+          dhall = pkgs.haskell.lib.dontCheck (super.callPackage ./overrides/dhall.nix {});
         };
       };
     };
